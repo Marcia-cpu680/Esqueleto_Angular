828b1bac1c433aa57a064fa0033400b00666deae
marcia galvez <marcia.galvez.rodriguez@cftmail.cl>
2024-10-13 23:16:16 -0300

    mi primer proyecto angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"contactate",
+        component: ContactoComponent
+    },
+    {
+        path:"inicio",
+        component:InicioComponent
+    },
+    {
+        path:"registrate",
+        component:RegistroComponent
+    }
+
+        
+];
